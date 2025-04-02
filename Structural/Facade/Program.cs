64a2de0dfd6ba46bf76093092c9dfef1cc3bdc65
Vinicius Lopes de Almeida <termobin@gmail.com>
2025-04-02 16:22:40 -0300

    Added comment to Facade pattern

--- Structural/Facade/Program.cs
+++ Structural/Facade/Program.cs
@@ -17,4 +17,5 @@ var facade = new OrderFacade(
 
 var orderDetail = new OrderDetails(1234, 1, 10M, "teste@email.com", "testUser", 335);
 
+// Aqui está o objetivo final, encapsular todo o processo de compra em um único ponto central, responsável por orquestrar a ordem de execução dos diferentes serviços que compoem este processamento.
 facade.ProcessOrders(orderDetail);
